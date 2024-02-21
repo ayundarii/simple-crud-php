d841df24d83d3e08f0ac8b2fa4911076f7fdf48f
ayundarii <ayu13pradnyandari@gmail.com>
2024-02-21 10:30:40 +0800

    edit delete.php

--- view/delete.php
+++ view/delete.php
@@ -6,7 +6,7 @@ $productController = new ProductController();
 
 $id = $_GET['id'];
 
-// Validate ID
+// validasi ID
 if (empty($id)) {
     header("Location: ../index.php?message=Product+ID+not+provided.");
     exit;
@@ -15,7 +15,7 @@ if (empty($id)) {
     exit;
 }
 
-// Check if product exists before attempting deletion
+// sebelum di delete cek apakah product dengan id tsb ada atau tidak
 $product = $productController->getProductById($id, TABLE_NAME);
 if (!$product) {
     header("Location: ../index.php?message=Product+not+found.");
@@ -27,44 +27,3 @@ if (!$product) {
     }
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Delete Product</title>
-</head>
-<body>
-    <h2>Delete Product</h2>
-    <a href="../index.php">Back to Product List</a>
-    <br><br>
-        <p>Are you sure you want to delete the following product?</p>
-        <table>
-            <tr>
-                <td>ID:</td>
-                <td><?php echo $product["id"]; ?></td>
-            </tr>
-            <tr>
-                <td>Product Name:</td>
-                <td><?php echo $product["product_name"]; ?></td>
-            </tr>
-            <tr>
-                <td>Price:</td>
-                <td><?php echo $product["price"]; ?></td>
-            </tr>
-            <tr>
-                <td>Quantity:</td>
-                <td><?php echo $product["quantity"]; ?></td>
-            </tr>
-            <tr>
-                <td>Description:</td>
-                <td><?php echo $product["description"]; ?></td>
-            </tr>
-            <form action="delete.php" method="get">
-                <input type="hidden" name="id" value="<?php echo $product['id']; ?>">
-                <input type="submit" value="Delete Product">
-            </form>
-        </table>
-</body>
-</html>
